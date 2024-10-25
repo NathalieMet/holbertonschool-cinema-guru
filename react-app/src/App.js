622b1c60563980f54df0f50ae509cbb2d2b008ef
nathalie <nathalie.metreau@hotmail.fr>
2024-10-25 15:15:09 +0200

    add task 3

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,52 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Dashboard from './Dashboard';
+import Authentication from './Authentication';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      const fetchAuth = async () => {
+        try {
+          const response = await fetch('/api/auth/', {
+            method: 'POST',
+            headers: {
+              'Authorization': `Bearer ${accessToken}`,
+            },
+          });
+
+          if (!response.ok) {
+            throw new Error('Server response was not ok');
+          }
+
+          const result = await response.json();
+          setIsLoggedIn(true);
+          setUserUsername(result.username);
+
+        } catch (error) {
+          console.error("Erreur d'authentification :", error);
+          setIsLoggedIn(false);
+        }
+      };
+
+      fetchAuth();
+    }
+  }, []);
+
+return (
+  <div className="App">
+    {isLoggedIn ? (
+      <Dashboard username={userUsername}></Dashboard>
+    ) : (
+      <Authentication></Authentication>
+    )}
+  </div>
+);
 }
 
 export default App;
