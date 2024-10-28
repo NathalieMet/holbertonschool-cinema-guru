4734134d8eccfd75d65f2aac11596e68dd3c03ba
nathalie <nathalie.metreau@hotmail.fr>
2024-10-28 15:15:01 +0100

    add task 8

--- react-app/src/routes/dashboard/Dashboard.js
+++ react-app/src/routes/dashboard/Dashboard.js
@@ -1,17 +1,29 @@
 import './dashboard.css'
 import Header from '../../components/navigation/Header'
 import SideBar from '../../components/navigation/SideBar';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from './HomePage';
+import Favorites from './Favorites';
+import WatchLater from './WatchLater';
 
 const Dashboard = ({ userUsername, setIsLoggedIn }) => {
 
 	return (
-		<div className="dashboard">
-			<Header
-			userUsername={userUsername}
-			setIsLoggedIn={setIsLoggedIn}>
-			</Header>
-			<SideBar></SideBar>
-		</div>
+		<BrowserRouter>
+			<div className="dashboard">
+				<Header
+					userUsername={userUsername}
+					setIsLoggedIn={setIsLoggedIn}>
+				</Header>
+				<SideBar></SideBar>
+				<Routes>
+					<Route path="/home" element={<HomePage />} />
+					<Route path="/favorites" element={<Favorites />} />
+					<Route path="/watchlater" element={<WatchLater />} />
+					<Route path="*" element={<Navigate to="/home" />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 };
 
